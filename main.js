f383e06d65f7255c6ad57f42a4bae003a3aaefa8
Abdul latif25 <alatiff045@gmail.com>
2024-06-03 10:43:05 +0700

    menambah main.js

---
+++ main.js
@@ -0,0 +1,52 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
+import {
+  getFirestore,
+  collection,
+  addDoc,
+  getDocs,
+  getDoc,
+  deleteDoc,
+  doc,
+  query,
+  orderBy,
+  updateDoc
+} from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
+
+// konfigurasi firabase
+const firebaseConfig = {
+  apiKey: "AIzaSyBlahoJjeK0jyO-4tZlAiPRjym6Mxn2P6o",
+  authDomain: "insan-cemerlang-59727.firebaseapp.com",
+  projectId: "insan-cemerlang-59727",
+  storageBucket: "insan-cemerlang-59727.appspot.com",
+  messagingSenderId: "839220708273",
+  appId: "1:839220708273:web:4d1dde85cf74aebd1d7390",
+  measurementId: "G-1VP3D59R0T"
+};
+
+// Inisialisasi Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+export async function ambilDaftarPembeli() {
+  const refDokumen = collection(db, "Pembeli");
+  const kueri = query(refDokumen, orderBy("nama"));
+  const cuplikanKueri = await getDocs(kueri);
+
+  let hasil = [];
+  cuplikanKueri.forEach((dok) => {
+    hasil.push({
+      id: dok.id,
+      nama: dok.data().nama,
+      alamat: dok.data().alamat,
+      noTlpon: dok.data().noTlpon,
+    });
+  });
+
+
+  return hasil;
+}
+
+// fungsi untuk menformat angka dengan pemisah ribuan pakai titik
+export function formatAngka(x) {
+  return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
+}
